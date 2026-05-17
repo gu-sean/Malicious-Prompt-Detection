ef7c7793ed05b731522873a522bd4b4630a03f73
leedohyun <wonehgus1234@naver.com>
2026-05-17 14:12:21 +0900

    Updated Changes

--- server/app/core/database.py
+++ server/app/core/database.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sess
 from sqlalchemy.orm import DeclarativeBase
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "mysql+aiomysql://user:user_password@db/prompt_guard")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite+aiosqlite:///./test.db")
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
